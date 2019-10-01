f6f200126c834e92150cf1e410252a0d9645c2a8
Zinc Glaze <graves.ezekiel@gmail.com>
2019-10-01 17:18:36 -0400

    initial commit

---
+++ copySelectedFiles.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+
+let searchIds = fs.readFileSync(`${__dirname}/fileIds.txt`, 'utf8').trim().split(',');
+let counter = 0;
+
+const traverseFileSystem = function (currentPath) {
+  //console.log('Current path:', currentPath);
+  let files = fs.readdirSync(currentPath);
+  
+  //traverse file system recursively
+  for (let i in files) {
+    let currentFile = files[i];
+    let currentFilePath = currentPath + '/' + files[i];
+    let stats = fs.statSync(currentFilePath);
+    //look at current file
+    if (stats.isFile()) {
+      //console.log('Current file: ', currentFilePath);
+      //iterate through array of file ids to be copied
+      for (let i in searchIds) {
+        //if current file meets the search criteria, copy it, and give confirmation
+        if (currentFilePath.includes('my-new-file') && currentFilePath.includes('.txt') && currentFilePath.includes(searchIds[i])) {
+          fs.copyFileSync(currentFilePath, `../file-mover-destination/${currentFile}`);
+          counter += 1;
+          console.log('Copied: ', currentFile);
+        }
+      }
+    } else if (stats.isDirectory()) {
+      traverseFileSystem(currentFilePath);
+    }
+  }
+};
+
+traverseFileSystem(__dirname);
+console.log(`${counter} files copied`);
